c2a057d43a2b2c7f11975fd825866965c6aa9313
plinadev <pavlenkopolina415@gmail.com>
2025-07-17 18:54:01 +0300

    Implement message sending, retrieval, and conversation listing functionality

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,111 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import prisma from "../db/prisma.js";
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    const { id: recieverId } = req.params;
+    const senderId = req.user.id;
+
+    let conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, recieverId],
+        },
+      },
+    });
+    if (!conversation) {
+      conversation = await prisma.conversation.create({
+        data: {
+          participantIds: {
+            set: [senderId, recieverId],
+          },
+        },
+      });
+    }
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        body: message,
+        conversationId: conversation.id,
+      },
+    });
+
+    if (newMessage) {
+      conversation = await prisma.conversation.update({
+        where: {
+          id: conversation.id,
+        },
+        data: {
+          messages: {
+            connect: {
+              id: newMessage.id,
+            },
+          },
+        },
+      });
+    }
+
+    res.status(StatusCodes.CREATED).json(newMessage);
+  } catch (err: any) {
+    console.log("Error in sendMessage controller", err.message);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Something went wrong" });
+  }
+};
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: recieverId } = req.params;
+    const senderId = req.user.id;
+    const conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, recieverId],
+        },
+      },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+      },
+    });
+    if (!conversation) {
+      return res.status(StatusCodes.OK).json([]);
+    }
+    res.status(StatusCodes.OK).json(conversation.messages);
+  } catch (err: any) {
+    console.log("Error in getMessages controller", err.message);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Something went wrong" });
+  }
+};
+
+export const getConversations = async (req: Request, res: Response) => {
+  try {
+    const currentUserId = req.user.id;
+    const users = await prisma.user.findMany({
+      where: {
+        id: {
+          not: currentUserId,
+        },
+      },
+      select: {
+        id: true,
+        fullName: true,
+        profilePic: true,
+      },
+    });
+    res.status(StatusCodes.OK).json(users);
+  } catch (err: any) {
+    console.log("Error in getConversations controller", err.message);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Something went wrong" });
+  }
+};
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import protectRoute from "../middlewares/protectRoute.js";
+import { getConversations, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/conversations", (req, res) => {
-  res.send("Conversation route");
-});
-
+router.get("/conversations", protectRoute, getConversations);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
